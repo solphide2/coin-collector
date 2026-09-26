a8474513b7861f9b9ae77d128ce69d31ff403723
sophie-yun <sophienuy1@gmail.com>
2026-09-25 20:20:30 -0400

    carrot counter
    added/displayed counter for carrots
    carrot disappears after counter reaches 5

--- Assets/Scripts/random_carrot_generation.cs
+++ Assets/Scripts/random_carrot_generation.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using TMPro;
 
 public class random_carrot_generation : MonoBehaviour
 {
@@ -6,11 +7,38 @@ public class random_carrot_generation : MonoBehaviour
     public float minY= -5.0f;
     public float maxX = 8.88f;
     public float maxY = 5.0f;
+    public int counter = 0;
+
+    public TextMeshProUGUI scoreText;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-       
+       UpdateCounter();
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        // Check if the object touching the carrot is tagged "Player"
+        if (other.CompareTag("Player"))
+        {
+            ChangePosition();
+            counter++;
+            UpdateCounter();
+            
+        }
+    }
+
+    void CarrotCollector() {
+        counter++;
+        UpdateCounter();
+        if (counter==5) {
+            gameObject.SetActive(false);
+        }
+        else {
+            ChangePosition();
+        }
+        
     }
 
     void ChangePosition()
@@ -18,17 +46,29 @@ public class random_carrot_generation : MonoBehaviour
         float randomx = Random.Range(minX, maxX);
         float randomy = Random.Range(minY, maxY);
         Debug.Log("Generated Math -> X: " + randomx + " | Y: " + randomy);
+        Debug.Log(counter);
         transform.position = new Vector3(randomx, randomy, transform.position.z);
         
+    }
+
+    void UpdateCounter()
+    {
+        if (scoreText!=null){
+            scoreText.text = "x" + counter;
+        }
+
     }
 
     // Update is called once per frame
     void Update()
     {
-         if (Input.GetKeyDown(KeyCode.Space)) {
+        if (Input.GetKeyDown(KeyCode.Space)) {
             Debug.Log("change");
-            ChangePosition();
+            CarrotCollector();
         }
         
+        }
+         
+        
     }
-}
+
